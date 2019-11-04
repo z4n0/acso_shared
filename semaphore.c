7155c6a3ae2b8b8de5796998a6a47f61b9cc1dd8
z4n0 <luca.zanotto@mail.polimi.it>
2019-11-04 22:07:44 +0100

    new

--- semaphore.c
+++ semaphore.c
@@ -2,13 +2,17 @@
 #include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+//non funzia
 #define NITER 1000000
 
 int cnt = 0;
+sem_t semname;
+//unsigned int n=1;
+
 
 void * Count(void * a)
 {
+    sem_wait(&semname);
     int i, tmp;
     for(i = 0; i < NITER; i++)
     {
@@ -16,17 +20,21 @@ void * Count(void * a)
         tmp = tmp+1;    /* increment the local copy */
         cnt = tmp;      /* store the local value into the global cnt */ 
     }
+    sem_post(&semname);
 }
+
 
 int main(int argc, char * argv[])
 {
     pthread_t tid1, tid2;
+    sem_init(&semname,0,1);
 
     if(pthread_create(&tid1, NULL, Count, NULL))
     {
       printf("\n ERROR creating thread 1");
       exit(1);
     }
+    sleep(1);
 
     if(pthread_create(&tid2, NULL, Count, NULL))
     {
@@ -50,6 +58,6 @@ int main(int argc, char * argv[])
         printf("\n BOOM! cnt is [%d], should be %d\n", cnt, 2*NITER);
     else
         printf("\n OK! cnt is [%d]\n", cnt);
-  
+    sem_destroy(&semname);
     pthread_exit(NULL);
 }
